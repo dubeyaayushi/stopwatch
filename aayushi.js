4e98eefecae0bf6252a33b8bb6530b0c2c7b5874
dubeyaayushi1028 <dubeyaayushi1028@gmail.com>
2023-07-01 19:52:23 +0530

    Adding new files

---
+++ aayushi.js
@@ -0,0 +1,18 @@
+//learning javascript
+//Year = prompt('Enter your Year')
+//Fees = prompt('Enter your one year fees')
+//Total_Amount = Fees * Year
+//onsole.log(Total_Amount)
+
+
+
+
+
+
+
+
+//functions
+function saymyname(name){
+    console.log('what is your name')
+}
+saymyname('pikabu')
